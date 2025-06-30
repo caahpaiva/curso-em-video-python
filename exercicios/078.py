d0123892ff02d7953f4699b1f2a578aa3f9b3561
caahpaiva <caahpaiva@live.com>
2025-06-30 17:00:01 -0300

    exercicios 078-079

--- exercicios/078.py
+++ exercicios/078.py
@@ -8,22 +8,32 @@ valores = []
 for c in range(0,5):
     valores.append(int(input(f'Digite um valor para a poição {c}: ')))
 
+maior = menor = valores[0]
+posicao_maior = []
+posicao_menor = []
 
 for c, valor in enumerate(valores):
-    if c == 0:
+    if menor > valor:
         menor = valor
+        posicao_menor = [c]
+    elif menor == valor:
+        posicao_menor.append(c)
+
+    if maior < valor:
         maior = valor
-        posicao_maior = c 
-        posicao_menor = c 
-    else: 
-        if menor >= valor:
-                menor = valor
-                posicao_menor = c
-        if maior <= valor:
-                maior = valor
-                posicao_maior = c
+        posicao_maior = [c]
+    elif maior == valor:
+        posicao_maior.append(c)
 
 print('=-'*30)
 print(f'Você digitou os valores {valores}')
-print(f'O maior valor digitado foi {maior} nas posições {posicao_maior}...')
-print(f'O menor valor digitado foi {menor} nas posições {posicao_menor}...')
+
+print(f'O maior valor digitado foi {maior} nas posições ', end ='')
+
+for valor in posicao_maior:
+    print(f'{valor}... ', end ='')
+
+print(f'\nO menor valor digitado foi {menor} nas posições ', end ='')
+
+for valor in posicao_menor:
+    print(f'{valor}... ', end ='')
--- exercicios/079.py
+++ exercicios/079.py
@@ -3,3 +3,25 @@ Crie um programa onde o usuário possa digitar vários valores númericos e
 cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 '''
+
+
+valores = []
+
+opcao = 'S'
+while opcao in 'S':
+    valor = (int(input(f'Digite um valor: ')))
+    if valor not in valores:
+        valores.append(valor) 
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+         
+
+    opcao = str(input('Quer continuar? [S/N]:')).strip().upper()[0]
+
+valores.sort()
+
+print('Você digitou os valores: ', end='')
+for num in valores:
+     print(f'{num}... ', end ='' )
+    
