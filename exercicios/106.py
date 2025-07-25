9ef45be7dd5d812d08c1b0d32a548c67af570590
caahpaiva <caahpaiva@live.com>
2025-07-25 16:33:30 -0300

    fix exercicio 106

--- exercicios/106.py
+++ exercicios/106.py
@@ -6,7 +6,7 @@ programa se encerrará. OBS: use cores.
 from time import sleep
 
 c = ('\033[m',           # 0 - sem cores
-     '\033[0;30;41m',     # 1 - vermelho
+     '\033[0;30;41m',    # 1 - vermelho
      '\033[0;30;42m',    # 2 - verde
      '\033[0;30;43m',    # 3 - amarelo
      '\033[0;30;44m',    # 4 - azul
